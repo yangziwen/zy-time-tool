dc2f3aeecfcfedcef58dc0673acf9d82ac1b6ce8
yangziwen <yangziwen@meituan.com>
2026-03-05 09:50:46 +0800

    修复右键菜单取时间的功能

--- js/background.js
+++ js/background.js
@@ -1,47 +1,78 @@
 var selectionTimestamp;
-
 var currentTimestamp = '';
-
 var currentDatetime = '';
 
-chrome.contextMenus.create({
-    'id':'zy_time_tool_menu',
-    'type':'normal',
-    'title':'提取时间信息',
-    'contexts':['selection'],
-    'onclick': (info) => {
-        if (!info || !info.selectionText) {
-            alert('选中内容无效');
-            return;
-        }
-        var text = info.selectionText.trim();
-        if (/^(\d{4})-(\d{2})-(\d{2})(?:\s(\d{2})\:(\d{2})\:(\d{2}))?$/.test(text)) {
-            var time = moment(text);
-            var timestamp = time.format('x');
-            var datetime = text;
-            alert(`选中时间为 [ ${datetime} ]\n对应的时间戳为 [ ${timestamp} ]`);
-            selectionTimestamp = timestamp;
-            currentTimestamp = timestamp;
-            currentDatetime = datetime;
-            return;
-        }
-        if (/^\d+$/.test(text)) {
-            var time = moment(parseInt(text));
-            var timestamp = text;
-            var datetime = time.format('YYYY-MM-DD HH:mm:ss');
-            alert(`选中的时间戳为 [ ${timestamp} ]\n对应的时间为 [ ${datetime} ]`);
-            selectionTimestamp = timestamp;
-            currentTimestamp = timestamp;
-            currentDatetime = datetime;
-            return;
-        }
+// Manifest V3 要求在 onInstalled 中创建上下文菜单
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.contextMenus.create({
+        'id': 'zy_time_tool_menu',
+        'type': 'normal',
+        'title': '提取时间信息',
+        'contexts': ['selection']
+    });
+});
         
-        alert(`选中的内容 [ ${text} ] 不是时间信息`);
-
+// 使用 onClicked 监听器替代 onclick 属性
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    if (!info || !info.selectionText) {
+        return;
     }
+    var text = info.selectionText.trim();
+
+    // 检测日期时间格式 yyyy-MM-dd HH:mm:ss
+    if (/^(\d{4})-(\d{2})-(\d{2})(?:\s(\d{2})\:(\d{2})\:(\d{2}))?$/.test(text)) {
+        // 使用原生 Date 解析日期
+        var date = new Date(text);
+        var timestamp = date.getTime().toString();
+        var datetime = text;
+        selectionTimestamp = timestamp;
+        currentTimestamp = timestamp;
+        currentDatetime = datetime;
+
+        // 通知用户 - 通过 content script
+        notifyUser(tab.id, `选中时间为 [ ${datetime} ]\n对应的时间戳为 [ ${timestamp} ]`);
+        return;
+    }
+
+    // 检测时间戳格式
+    if (/^\d+$/.test(text)) {
+        var timestamp = text;
+        var date = new Date(parseInt(text));
+        var year = date.getFullYear();
+        var month = String(date.getMonth() + 1).padStart(2, '0');
+        var day = String(date.getDate()).padStart(2, '0');
+        var hours = String(date.getHours()).padStart(2, '0');
+        var minutes = String(date.getMinutes()).padStart(2, '0');
+        var seconds = String(date.getSeconds()).padStart(2, '0');
+        var datetime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+        selectionTimestamp = timestamp;
+        currentTimestamp = timestamp;
+        currentDatetime = datetime;
+
+        // 通知用户
+        notifyUser(tab.id, `选中的时间戳为 [ ${timestamp} ]\n对应的时间为 [ ${datetime} ]`);
+        return;
+    }
+
+    // 不是时间格式
+    notifyUser(tab.id, `选中的内容 [ ${text} ] 不是时间信息`);
 });
 
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
+// 通过 content script 通知用户
+function notifyUser(tabId, message) {
+    chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        func: function(msg) {
+            alert(msg);
+        },
+        args: [message]
+    }).catch(function(err) {
+        console.log('无法显示提示:', err);
+    });
+}
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     if (message == 'current_selection_time') {
         sendResponse(selectionTimestamp);
         return;
